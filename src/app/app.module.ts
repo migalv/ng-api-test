a02ef58cc77bbcb0c369d2dee520272439a1285b
migalv <migalv007@gmail.com>
2021-05-19 17:41:45 +0200

    Añadirá Angular Material al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { apiBecarios } from './services/api_becarios.service';
 
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
