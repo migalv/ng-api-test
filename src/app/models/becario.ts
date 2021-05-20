6a1fe9271c46f0fb1bb33004356725ebc1c46472
migalv <migalv007@gmail.com>
2021-05-20 14:37:46 +0200

    Control the missing data case in the UI

--- src/app/models/becario.ts
+++ src/app/models/becario.ts
@@ -17,12 +17,12 @@ export class Becario {
     responsables: string[],
     fechaalta: string,
   ) {
-    this.id = id == "" ? "N/A" : id;
-    this.nombre = nombre == "" ? "N/A" : nombre;
-    this.apellidos = apellidos == "" ? "N/A" : apellidos;
-    this.puesto = puesto == "" ? "N/A" : puesto;
-    this.horario = horario == "" ? "N/A" : horario;
-    this.responsables = responsables == [] ? ["N/A"] : responsables;
-    this.fechaalta = fechaalta == "" ? "N/A" : fechaalta
+    this.id = id;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.puesto = puesto;
+    this.horario = horario;
+    this.responsables = responsables;
+    this.fechaalta = fechaalta
   }
 }
--- src/app/services/api_becarios.service.ts
+++ src/app/services/api_becarios.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+const base_url = "https://guadaltech-fullstack.herokuapp.com/api"
 
 @Injectable()
 export class apiBecarios {
@@ -9,7 +10,6 @@ export class apiBecarios {
   constructor(private httpClient: HttpClient) { }
 
   getBecarios(): Observable<any> {
-    // TODO: CAMBIAR LA BASE DE LA API A UNA VARIABLE CONSTANTE
-    return this.httpClient.get("https://guadaltech-fullstack.herokuapp.com/api/becarios/")
+    return this.httpClient.get(base_url + "/becarios")
   }
 }
